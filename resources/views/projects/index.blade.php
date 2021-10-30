61c29b6b962e71d467f5abb4dfed0c877a687c82
Nurul Izzatil Ulum <76694068+nurululum15@users.noreply.github.com>
2021-10-31 02:10:20 +0700

    CRUD

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Laravel 8 CRUD </h2>
+                <h2>My Projects </h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-success" href="{{ route('projects.create') }}" title="Create a project"> <i class="fas fa-plus-circle"></i>
